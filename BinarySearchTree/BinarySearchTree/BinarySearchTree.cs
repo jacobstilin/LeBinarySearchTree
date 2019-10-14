239b111909ef2fb969556963aa8cd81985d378d0
Jacob Stilin <stilinj@gmail.com>
2019-10-14 16:04:22 -0500

    Init

---
+++ BinarySearchTree/BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class BinarySearchTree
+    {
+        public Node root;
+
+        public BinarySearchTree()
+        {
+            root = null;
+        }
+
+        public Node returnNode()
+        {
+            return root;
+        }
+
+        
+        public void Add(int id)
+        {
+            Node newNode = new Node();
+            newNode.item = id;
+
+            if (root == null)
+            {
+                root = newNode;
+            }
+
+            else
+            {
+                Node current = root;
+                Node parent;
+                while (true)
+                {
+                    parent = current;
+                    if (id < current.item)
+                    {
+                        current = current.left;
+                        if (current == null)
+                        {
+                            parent.left = newNode;
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        current = current.right;
+                        if (current == null)
+                        {
+                            parent.right = newNode;
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+
+        public bool Search(int term)
+        {
+            bool result = RecursiveSearch(term, root);
+            return result;
+        }
+
+        public bool RecursiveSearch(int term, Node middle)
+        {
+            if (term == middle.item)
+            {
+                return true;
+            }
+
+            else if (term < middle.item)
+            {
+                if (middle.left == null)
+                {
+                    return false;
+                }
+                
+                else
+                {
+                    return RecursiveSearch(term, middle.left);
+                }
+            }
+            else
+            {
+                if (middle.right == null)
+                {
+                    return false;
+                }
+                
+                else
+                {
+                    return RecursiveSearch(term, middle.right);
+                }
+            }
+        }
+    }
+}
